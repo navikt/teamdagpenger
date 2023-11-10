e6e0b44de81743ad2c4ded61a0d3ada207ac1bc3
Abdifatah Bashi <abdifatah.bashi@nav.no>
2023-11-10 15:25:51 +0100

    Oppdater edit-url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -50,8 +50,7 @@ const config = {
           sidebarPath: "./sidebars.js",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/navikt/teamdagpenger",
         },
         theme: {
           customCss: "./src/css/custom.css",
